fa70911bf5eb16d63f8c0684a71f7cd6f7ddd9a2
Dennis Mithamo <dennisbmithamo@gmail.com>
2020-01-03 00:34:28 +0300

    feature: implement theming using the context api

--- src/contexts/ThemeContext.jsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-const ThemeContext = React.createContext('light');
-export default function ThemeContextProvider() {
-  return <ThemeContext.Provider value="light" />;
-}
